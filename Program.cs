e5cd5378fa33c488c617b7cb0ea1345afab469f7
Francisco Yucovsky <72484079+yucovsky@users.noreply.github.com>
2025-06-16 10:42:59 -0300

    Update Program.cs
    borre el print de carrito en proceso

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
         var tienda = new Tienda();
         tienda.inicializarDatos();
-        Console.WriteLine("carrito en proceso");
+        tienda.mostrarMenu();
     }
 }
