37cc3e4ec07e3dbc26f4ef2fee2b2bffc849f7f5
Francisco Yucovsky <72484079+yucovsky@users.noreply.github.com>
2025-06-16 16:05:16 -0300

    Faltan IVA y descuentos

--- CarritoCompras/Carrito.cs
+++ CarritoCompras/Carrito.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace CarritoCompras
 {
-    class Carrito
+    public class Carrito
     {
+        public List<ItemCarrito> items { get; } = new List<ItemCarrito>();
+
+        public void agregarItem(Producto producto, int cantidad)
+        {
+            var itemExistente = items.FirstOrDefault(i => i.producto.codigo == producto.codigo);
+
+            if (itemExistente != null)
+            {
+                itemExistente.cantidad += cantidad;
+            }
+            else
+            {
+                items.Add(new ItemCarrito { producto = producto, cantidad = cantidad });
+            }
+        }
+
+        public decimal calcularTotal()
+        {
+            // agregar iva
+            return items.Sum(item => item.calcularSubtotal());
+        }
+
+        public void eliminarItem(int codigoProducto)
+        {
+            var item = items.FirstOrDefault(i => i.producto.codigo == codigoProducto);
+            if (item != null)
+            {
+                items.Remove(item);
+            }
+        }
     }
 }
