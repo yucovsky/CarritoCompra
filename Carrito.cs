482de59a26fe2a8cc9407e4a0d39d2a3234c45f6
Francisco Yucovsky <72484079+yucovsky@users.noreply.github.com>
2025-06-16 10:36:44 -0300

    método agregar y eliminar (a chequear)

--- Carrito.cs
+++ Carrito.cs
@@ -4,19 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Carrito
+namespace CarritoCompras
 {
-    public List<ItemCarrito> items { get; } = new List<ItemCarrito>();
-
-    public void agregarItem(Producto producto, int cantidad)
+    public class Carrito
     {
-        //stock
-        items.Add(new ItemCarrito { producto = producto, cantidad = cantidad });
-    }
+        public List<ItemCarrito> items { get; } = new List<ItemCarrito>();
 
-    public decimal calcularTotal()
-    {
-        // iva y descuentos
-        return items.Sum(item => item.producto.precio * item.cantidad);
+        public void agregarItem(Producto producto, int cantidad)
+        {
+            var itemExistente = items.FirstOrDefault(i => i.producto.codigo == producto.codigo);
+
+            if (itemExistente != null)
+            {
+                itemExistente.cantidad += cantidad;
+            }
+            else
+            {
+                items.Add(new ItemCarrito { producto = producto, cantidad = cantidad });
+            }
+        }
+
+        public decimal calcularTotal()
+        {
+            // agregar iva
+            return items.Sum(item => item.calcularSubtotal());
+        }
+
+        public void eliminarItem(int codigoProducto)
+        {
+            var item = items.FirstOrDefault(i => i.producto.codigo == codigoProducto);
+            if (item != null)
+            {
+                items.Remove(item);
+            }
+        }
     }
 }
